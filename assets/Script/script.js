d4110e8a40a6069aec5c7253194d8bf275769b8c
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-03-30 22:47:54 -0300

    add pasta de imagens, modifiquei o css e js

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,4 +1,8 @@
 const body = document.getElementsByTagName('body')[0];
-body.onload = function(){
-    console.log('OLA MUNDO!')
-}
+body.onload = function () {
+	// Abrirá de acordo com o link incerido
+	const pricing = document.getElementById('images');
+	pricing.addEventListener('click', function () {
+		window.open('https://github.com/JoaoPedroAC/Interactive-Pricing-Component');
+	});
+};
