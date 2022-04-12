13fa7aebad2a34caba8ec955e6243d8438b67410
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-04-11 22:13:35 -0300

    modifiquei o JS

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,8 +1,18 @@
-const body = document.getElementsByTagName('body')[0];
-body.onload = function () {
-	// Abrirá de acordo com o link incerido
-	const pricing = document.getElementById('images');
-	pricing.addEventListener('click', function () {
-		window.open('https://github.com/JoaoPedroAC/Interactive-Pricing-Component');
-	});
+window.onload = function () {
+	const manyImages = document.querySelectorAll('#project-blocks');
+	const images = document.getElementsByTagName('img');
+	const siteList = [
+		'https://github.com/JoaoPedroAC/Interactive-Pricing-Component',
+		'https://github.com/JoaoPedroAC/Calculator',
+		'https://github.com/JoaoPedroAC/TODO-LIST',
+	];
+	for (let someImages of manyImages) {
+		someImages.addEventListener('click', function () {
+			for (let indexImage in manyImages) {
+				window.open(`${siteList[indexImage]}`);
+			}
+		});
+	}
 };
+
+/* bug ao abrir o link das imagens */
