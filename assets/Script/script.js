5c55b7bd350252e48a7bf8aaa44eee142862f5f1
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-04-13 21:19:46 -0300

    arrumei o erro no JS e coloquei max-width no textarea

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,18 +1,13 @@
 window.onload = function () {
-	const manyImages = document.querySelectorAll('#project-blocks');
-	const images = document.getElementsByTagName('img');
+	const manyImages = document.querySelectorAll('.project-blocks img');
 	const siteList = [
 		'https://github.com/JoaoPedroAC/Interactive-Pricing-Component',
 		'https://github.com/JoaoPedroAC/Calculator',
 		'https://github.com/JoaoPedroAC/TODO-LIST',
 	];
-	for (let someImages of manyImages) {
-		someImages.addEventListener('click', function () {
-			for (let indexImage in manyImages) {
-				window.open(`${siteList[indexImage]}`);
-			}
+	for (let index=0; index<manyImages.length; index++) {
+		manyImages[index].addEventListener('click', function () {
+			window.open(`${siteList[index]}`);
 		});
 	}
 };
-
-/* bug ao abrir o link das imagens */
