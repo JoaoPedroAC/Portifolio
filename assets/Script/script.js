f8a492f98984f1549237a448262fba28f18d6154
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2022-04-26 21:51:30 -0300

    add button editei no css e coloquei obs JS

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,10 +1,12 @@
 window.onload = function () {
 	const manyImages = document.querySelectorAll('.project-blocks img');
+	// array com os links dos repositorias de acordo com as imagens
 	const siteList = [
 		'https://github.com/JoaoPedroAC/Interactive-Pricing-Component',
 		'https://github.com/JoaoPedroAC/Calculator',
 		'https://github.com/JoaoPedroAC/TODO-LIST',
 	];
+	// acesso aos linka por tras das imagens
 	for (let index=0; index<manyImages.length; index++) {
 		manyImages[index].addEventListener('click', function () {
 			window.open(`${siteList[index]}`);
